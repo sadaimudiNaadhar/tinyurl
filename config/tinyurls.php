43153a254b120c6a40ffc0b56cc89e0570b205f2
Vaisakh V.N <42279368+sadaimudiNaadhar@users.noreply.github.com>
2018-12-23 22:11:01 +0530

    configurations

---
+++ config/tinyurls.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created By sadaimudiNaadhar 
+ * @author https://github.com/sadaimudiNaadhar
+ *
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Table  Name
+    |--------------------------------------------------------------------------
+    | This will be done before running migration
+    */
+
+    'TABLE' => 'tiny_urls',
+    'URL_PREFIX' => 'v1',
+    'MIN_LENGTH' => 4,
+    'USE_NUMBERS' => true,
+    'AUTH_URL_ROUTES' => false,
+];  
